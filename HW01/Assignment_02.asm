4bfc16e1a1065839d7107dd732c3dbf60444c57a
dr_nyt <autumnwind101011@gmail.com>
2020-02-05 11:45:06 +0400

    Update Assignment_02.asm

--- HW01/Assignment_02.asm
+++ HW01/Assignment_02.asm
@@ -1,6 +1,6 @@
 .data
 	prompt: .asciiz "Enter an integer to count to: "
-	enter: .asciiz "\n"
+	output: .asciiz "Total sum is: "
 	
 .text
 main:
@@ -12,25 +12,27 @@ main:
 	# Get integer
 	li $v0, 5
 	syscall
-	move $s0, $v0
+	move $s0, $v0			# Range
 	
-	addi $t0, $zero, 1		# i = 1
+	addi $t0, $zero, 0		# i = 0
+	addi $s1, $zero, 0		# sum = 0
 	
 	loop:
-		# Print i
-		move $a0, $t0
-		li $v0, 1
-		syscall
-		
-		# Add Space
-		la $a0, enter
-		li $v0, 4
-		syscall
-		
-		# Logic
-		beq $t0, $s0, exit	# Exit on complete
-		addi $t0, $t0, 1	# i += 1
-		j loop				# Loop back
+		addi $t0, $t0, 1	# i = i + 1
+		add $s1, $s1, $t0	# sum = sum + i
+		bne $t0, $s0, loop	# Loop back
+	
+	# Print output string
+	la $a0, output
+	li $v0, 4
+	syscall
+	
+	# Print Total Sum
+	move $a0, $s1
+	li $v0, 1
+	syscall
+	
+	j exit
 
 exit:
 	li $v0, 10
